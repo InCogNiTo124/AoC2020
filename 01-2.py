b52af46c6fb3edd44f6f124bd1428f28ad993261
InCogNiTo124 <marijansmetko123@gmail.com>
2020-12-02 01:03:30 +0100

    Fix day 1

--- 01-2.py
+++ 01-2.py
@@ -7,9 +7,9 @@ while True:
 
 for i in range(len(numbers)-2):
     x = numbers[i]
-    for j in range(i, len(numbers)-1):
+    for j in range(i+1, len(numbers)-1):
         y = numbers[j]
-        for k in range(j, len(numbers)):
+        for k in range(j+1, len(numbers)):
             z = numbers[k]
             if x+y+z == 2020:
                 print(x, y, z, x*y*z)
