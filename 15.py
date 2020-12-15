0c28926108173c6ac4fa4410b609b17865cfb713
InCogNiTo124 <marijansmetko123@gmail.com>
2020-12-15 14:12:56 +0100

    Day 15

---
+++ 15.py
@@ -0,0 +1,14 @@
+numbers = list(map(int, input().strip().split(',')))
+cache_dict = dict()
+for i, n in enumerate(numbers[:-1], 1):
+    cache_dict[n] = i
+
+n = numbers[-1]
+for i in range(len(numbers), 2020):
+    if n not in cache_dict:
+        number = 0
+    else:
+        number = i - cache_dict[n]
+    cache_dict[n] = i
+    n = number
+print(n)
